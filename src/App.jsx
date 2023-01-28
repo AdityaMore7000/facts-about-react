e412467699ecae3fc4a3e26467590b0d4fb1a27e
AdityaMore7000 <adi.more7000@gmail.com>
2023-01-28 20:58:38 +0530

    fixed background image

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from "./Components/Navbar"
 function App() {
   return (
     <div className="App">
-      <img id="bcimg" src="images/reactjs-icon-2.png" alt="bckgimg" />
       <Navbar/>
       <List/>
     </div>
