1d9e30072ca9573537f317c018d56b32d24c0e7a
AdityaMore7000 <adi.more7000@gmail.com>
2023-01-28 21:42:22 +0530

    fixed bcimg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar"
 function App() {
   return (
     <div className="App">
+      <img src="images/reactjs-icon-2.png" alt="..." id="bckg" />
       <Navbar/>
       <List/>
     </div>
