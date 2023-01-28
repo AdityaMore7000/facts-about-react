da7fb39bfde2338ac11202e0efe0f644ae5f607e
AdityaMore7000 <adi.more7000@gmail.com>
2023-01-28 15:07:06 +0530

    completed app

---
+++ src/Components/List.jsx
@@ -0,0 +1,16 @@
+const List = () => {
+    return ( 
+        <div className="list">
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>It is maintained by FaceBook</li>
+                <li>Powers thousands of enterprise apps, inclusing mobile apps</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default List;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <img src="images/React-icon.svg.png" alt="react-nav-icon" />
+            <h2>React Facts</h2>
+            <p>First React Project</p>
+        </div>
+     );
+}
+ 
+export default Navbar;
