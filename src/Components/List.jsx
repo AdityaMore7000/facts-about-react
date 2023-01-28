b65436d100b79a4dcc899cc2ff24140a6b5b8114
AdityaMore7000 <adi.more7000@gmail.com>
2023-01-28 22:22:48 +0530

    spelling mistake fixed

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -7,7 +7,7 @@ const List = () => {
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>It is maintained by FaceBook</li>
-                <li>Powers thousands of enterprise apps, inclusing mobile apps</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </div>
      );
